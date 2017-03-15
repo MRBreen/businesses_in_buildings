cf1e3558bc70a8d53654c00a8a4734ae675389a1
Mike Breen <breen.mr(at)gmail.com>
2017-03-14 18:49:57 -0700

    scrape loops through 50 records

--- src/scraping.py
+++ src/scraping.py
@@ -9,16 +9,16 @@ def sleep():
     time.sleep(5+random.random()*5)
 
 # from http://programminghistorian.org/lessons/output-data-as-html-file
-def write_html(street, serial):
+def write_html(street, serial=0):
     '''Writes HTML to a file in the data folder
     '''
     street = street
-    filename = '../data/' + street + '_' + serial '.html'
+    filename = '../data/' + street + '_' + serial + '.html'
     f = open(filename, 'w')
     f.write('../data/' + details.encode('utf-8'))
 
 
-def street_scrape(street_list)
+def street_scrape(street_list):
     '''Scrapes data from list of street
     '''
     for street in street_list:
@@ -32,28 +32,36 @@ def street_scrape(street_list)
         page = 0
         # address_scrape()
         #### might make another function here
+        sleep()
         links = browser.find_elements_by_css_selector("a.DocFieldLink")
         links = links[2:52]
         links = filter(None, links)
-        for link in links:
-            if link[i] != "":
-                links = browser.find_elements_by_css_selector("a.DocFieldLink")
-                links = links[2:52]   ##exclude links 0 and 1
-                links = filter(None, links)
-                links[i].click()
-                sleep()
-                details = browser.page_source
-                serial = page*50 + i
-                write_html (details, serial)
-                browser.back()
+        while i < 50:
+            sleep()
+            links = browser.find_elements_by_css_selector("a.DocFieldLink")
+            links = links[2:52]   ##exclude links 0 and 1
+            links = filter(None, links)
+            sleep()
+            links[i].click()
+            sleep()
+            details = browser.page_source
+            serial = str(page*50 + i)
+            write_html(details, street, serial)
+            sleep()
+            browser.back()
+            i+=1
+
 
     return None
 
 if __name__ == '__main__':
     browser = webdriver.Firefox()
     browser.get('http://bls.dor.wa.gov/')
+    sleep()
     blink = browser.find_element_by_link_text('Business licenses')
     blink.click()
+    sleep()
     new_window = browser.window_handles[1]
     browser.switch_to_window(new_window)
-    street_scrape('['jackson']')  # can point to data file of csv
+    sleep()
+    street_scrape(['jackson'])  # can point to data file of csv
