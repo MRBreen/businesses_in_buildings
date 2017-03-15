4189aa7dc0f92ad0c465d394cc34760f301881cc
Mike Breen <breen.mr(at)gmail.com>
2017-03-15 11:06:53 -0700

    builds class for extracting 4 field

--- src/extract.py
+++ src/extract.py
@@ -14,16 +14,58 @@ class extract(object):
     """An object which holds extracted values using BeautifulSoup
     """
     def __init__(self):
-        self._BeautifulSoup =
+        self.soup = None
+        self.business_name = None
         self.address_one = None
+        self.address_one = None
+        self.address_mailing = None
+        self.ubi = None
 
-    def address_one(soup)
-        address_one = soup.find(id='caption2_c-u')
-        if address_one is not None:
-            address_one = address_one.contents[0].encode('utf-8').strip()
+    def create_soup(self, filename):
+        folder = ('../data/')
+        with open (folder+filename) as pagefile:
+            page_source = pagefile.read()
+        self.soup = BeautifulSoup(page_source, 'html.parser')
+
+    def get_buisiness_name(self):
+        self.business_name = self.soup.find(id='caption2_c-e')
+        if self.business_name is not None:
+            self.business_name = self.business_name.contents[0].encode('utf-8').strip()
         else:
-            address_one=""
-        return(address_one)
+            self.business_name=""
+
+    def get_address_one(self):
+        self.address_one = self.soup.find(id='caption2_c-u')
+        if self.address_one is not None:
+            if len(self.address_one.contents) != 0:
+                self.address_one = self.address_one.contents[0].encode('utf-8').strip()
+        else:
+            self.address_one=""
+
+    def get_address_mailing(self):
+        self.address_mailing = self.soup.find(id='caption2_c-01')
+        if self.address_mailing is not None:
+            if len(self.address_mailing.contents) != 0:
+                self.address_mailing = self.address_mailing.contents[0].encode('utf-8').strip()
+        else:
+            self.address_mailing=""
+
+    def get_ubi(self):
+        self.ubi = self.soup.find(id='caption2_c-i')
+        if self.ubi is not None:
+            self.ubi = self.ubi.contents[0].encode('utf-8').strip()
+        else:
+            self.ubi=""
+
+    def build(self, filename):
+        self.create_soup(filename)
+        self.get_buisiness_name()
+        self.get_address_one()
+        #self.get_address_mailing()
+        self.get_ubi()
+
+    def write_json(self):
+        pass
 
 
 ##if __name__ == '__main__':
