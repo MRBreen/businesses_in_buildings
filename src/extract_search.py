95c397fc1d25689fed05e96dd62ed0303e78729c
Mike Breen <breen.mr(at)gmail.com>
2017-03-16 21:03:55 -0700

    added functions to get search count and links

--- src/extract_search.py
+++ src/extract_search.py
@@ -4,32 +4,77 @@ import requests
 import json
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError, CollectionInvalid
-
+from os import listdir
+from os.path import isfile, join
 
 db_cilent = MongoClient('mongodb://localhost:27017/')
 db = db_cilent['WBS']
 collections = db.biz
 
-def number_results(soup):
-resultstats = soup.find_all(id="resultStats")
-resultstat = resultstats[0]
-results_returned = resultstat.contents[0].split()[0]
-
-soup = BeautifulSoup(open(file), 'lxml')
 
 
-index=[]
-for i, link in enumerate(soup.find_all('a')):
-    if (link.get('href')) == '#':
-        index.append(i+2)
+class search_page(object):
+    """Holds extracted values
+    """
+    def __init__(self):
+        self.name = None
+        self.addr_results = None
+        self.addr_link = None
+        self.city_results = None
+        self.city_link = None
+        self.city_results_pres = None
+        self.city_link_pres = None
+        self.city_results_hista = None
+        self.city_link_hista = None
 
-links = []
-for i in index[:-1]:
-    links.append(soup.find_all('a')[i])
+    def create_soup(self, filename):
+        folder = ('../data_from_chromew/')
+        with open (folder+filename) as pagefile:
+            page_source = pagefile.read()
+        self.soup = BeautifulSoup(page_source, 'html.parser')
 
+    def get_num_results(self):
+        """ Gets the number of results
+        """
+        resultstats = self.soup.find_all(id="resultStats")
+        resultstat = resultstats[0]
+        self.addr_results = resultstat.contents[0].split()[0])
 
-    db.biz.insert_one({
+    def get_links(self):
+        index=[]
+        for i, link in enumerate(self.soup.find_all('a')):
+            if (link.get('href')) == '#':
+                index.append(i+2)
+        links = []
+        for i in index[:-1]:
+            links.append(soup.find_all('a')[i])
+        self.addr_link = links
+
+    def add_mongo(self):
+        db.biz.insert_one({
             "Bus_Name" : soup.title.text.split(' seattle - Google')[0],
-            "Name_city_returns" : results_returned,
-            "Name_city_links" : [x.get('href') for x in links],
+            "Name_city_returns" : self.addr_results,
+            "Name_city_links" : self.addr_link,
         })
+
+    def build(self, filename):
+        """Calls subfunction which create values for the parameters
+        arg: filename is html file to be extracted
+        """
+        self.create_soup(filename)
+        self.get_num_results()
+        self.get_links()
+
+if __name__ == '__main__':
+    """code processes all html files in data folder and
+    saves fields to records in mongoDB.
+    collection name is defined at the top of the code.
+    """
+    print "DB and collection is:" , collection.full_name
+    print "Initial record count:" , collection.count()
+    for file in os.listdir('../data_from_chrome/'):
+        if '.html' in file:
+            page_values = search_page()
+            page_values.build(file)
+            page_values.db_add(collection)
+    print "Final record count:" , collection.count()
