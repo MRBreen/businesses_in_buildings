ad27c59079faaaf7ff9adfcee8cf4b8a584a2529
Mike Breen <breen.mr(at)gmail.com>
2017-03-19 21:30:14 -0700

    script to run from aws

---
+++ scra8.sh
@@ -0,0 +1,322 @@
+python src/bings.py "BAN206 1ST AVE S STE 41098104"
+python src/bings.py "4G WIRELESS, INC.817 1ST AVE98104"
+python src/bings.py "BANKRUPTCY MANAGEMENT CONSULTANTS CORP.600 1ST AVE STE 62398104"
+python src/bings.py "BAR 309309 1ST AVE S98104"
+python src/bings.py "BATES LAW, PLLC600 1ST AVE STE 61998104"
+python src/bings.py "BEACON CONDOMINIUMS, LLC114 1/2 1ST AVE # 198104"
+pgrep phantomjs | xargs kill
+python src/bings.py "BEAST MODE APPAREL558 1ST AVE S  STE 10098104"
+python src/bings.py "BEBA'S DELI415 1ST AVE S98104"
+python src/bings.py "BECKETT LAW OFFICES, PLLC811 1ST AVE STE 20898104"
+python src/bings.py "BELBOY BUILDING, LLC114 1/2 1ST AVE S STE 198104"
+python src/bings.py "BELL FLEGENHEIMER P.S.119 1ST AVE S STE 50098104"
+python src/bings.py "7-ELEVEN #35541A627 1ST AVE98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "BEST LIVES, INC216 1ST AVE S STE 23498104"
+python src/bings.py "7-ELEVEN #35541B627 1ST AVE98104"
+python src/bings.py "BETTER TOGETHER VENTURES LLC600 1ST AVE  STE LL1598104"
+python src/bings.py "BEVYUP568 1ST AVE S STE 55098104"
+python src/bings.py "BID OF WISCONSIN LLC411 1ST AVE S STE 200N98104"
+python src/bings.py "BIDADOO, INC.558 1ST AVE S STE 10098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "BILL SPEIDEL ENTERPRISES, INC.614 1ST AVE  STE 40098104"
+python src/bings.py "BIOLOGICAL THERAPIES IN PSYCHIATRY1000 1ST AVE UNIT 190198104"
+python src/bings.py "BJ PATCH526 1ST AVE S APT 52098104"
+python src/bings.py "BLANTON TURNER614 1ST AVE FL 298104"
+python src/bings.py "BLUE WATER WEALTH, INC.605 1ST AVE STE 50698104"
+python src/bings.py "BLUE WAVE POLITICAL PARTNERS, LLC119 1ST AVE S STE 32098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "BRAVO!568 1ST AVE S STE 35098104"
+python src/bings.py "BRIAN ROESCH LAW OFFICE216 1ST AVE S STE 30098104"
+python src/bings.py "BRIGHT SPECTRUM, INC.219 1ST AVE S STE 22098104"
+python src/bings.py "BUILDPULSE INC.605 1ST AVE  STE 22098104"
+python src/bings.py "ABACUS RESOURCE MANAGEMENT COMPANY401 2ND AVE S STE 20198104"
+python src/bings.py "ACCEL SOLUTIONS GROUP, INC.801 2ND AVE STE 80098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ACCELCRM, LLC801 2ND AVE  STE 80098104"
+python src/bings.py "ACTIVE BUILDING, LLC615 2ND AVE STE 70098104"
+python src/bings.py "ACTIVE RESOURCES ,INC801 2ND AVE98104"
+python src/bings.py "ACTIVELY LEARN220 2ND AVE S98104"
+python src/bings.py "AD LIGHTNING INC.240 2ND AVE S  STE 30098104"
+python src/bings.py "ADAM SCOTT LAW719 2ND AVE STE 125098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ADAMANT STRATEGIES, LLC220 2ND AVE S UNIT 40898104"
+python src/bings.py "ADECCO ENGINEERING & TECHNOLOGY1002 2ND AVE STE 156098104"
+python src/bings.py "12TH AND PROCTOR INVESTMENTS LLC821 2ND AVE # 220098104"
+python src/bings.py "ADEPT CAPITAL CONCEPTS, INC.506 2ND AVE STE 140098104"
+python src/bings.py "13TH FLOOR, LLC705 2ND AVE STE 130098104"
+python src/bings.py "ADVANTEC CONSULTING ENGINEERS, INC.506 2ND AVE  STE 140098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ADVOCATE INSURANCE GROUP, LLC801 2ND AVE STE 115098104"
+python src/bings.py "AECOM TECHNICAL SERVICES, INC.710 2ND AVE98104"
+python src/bings.py "AF GRAPHICS506 2ND AVE #212798104"
+python src/bings.py "AG 47 STUDIO GALLERY163 S JACKSON ST 2ND FL98104"
+python src/bings.py "AID CAMBODIA705 2ND AVE STE 60198104"
+python src/bings.py "AIKEN ST LOUIS & SILJEG PS801 2ND AVE STE 120098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "AIRGENIUM, INC.801 2ND AVE STE 80098104"
+python src/bings.py "ALA, PUGET SOUND CHAPTER524 2ND AVE STE 50098104"
+pgrep phantomjs | xargs killpgrep phantomjs | xargs kill
+python src/scraping_run.py Alaskan 8
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Jackson 8
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Jackson 9
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Jackson 10
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Jackson 11
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Jackson 12
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Jackson 13
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Jackson 14
+pgrep phantomjs | xargs kill
+python src/scraping_run.py King 8
+pgrep phantomjs | xargs kill
+python src/scraping_run.py King 9
+pgrep phantomjs | xargs kill
+python src/scraping_run.py King 10
+pgrep phantomjs | xargs kill
+python src/scraping_run.py King 11
+pgrep phantomjs | xargs kill
+python src/scraping_run.py King 12
+pgrep phantomjs | xargs kill
+python src/scraping_run.py King 13
+pgrep phantomjs | xargs kill
+python src/scraping_run.py King 14
+python src/bings.py "1901 L.L.C.816 2ND AVE STE 30098104"
+python src/bings.py "ALASKA AFFILIATED CRAB HARVESTER ARBITRATION ASSOCIATION719 2ND AVE # 115098104"
+python src/bings.py "ALASKA BUILDING DELI & SUNDRIES624 2ND AVE98104"
+python src/bings.py "ALASKA CRAB PROCESSORS ARBITRATION ORGANIZATION719 2ND AVE STE 115098104"
+python src/bings.py "1ENERGY SERVICES, LLC615 2ND AVE  STE 62098104"
+python src/bings.py "1ENERGY SYSTEMS, INC.615 2ND AVE  STE 62098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ALL TREATMENT1000 2ND AVE STE 370098104"
+python src/bings.py "ALLIANCE OF ANGELS719 2ND AVE STE 140398104"
+python src/bings.py "ALL-STAR ORCHESTRA719 2ND AVE STE 115098104"
+python src/bings.py "ALLURE528 2ND AVE98104"
+python src/bings.py "ALLY LEGAL, INC. P.S.705 2ND AVE STE 60598104"
+python src/bings.py "ALLYCE WOOD STUDIOS420 2ND AVENUE EXT S APT 20898104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ALLYDENT LAB INTERNATIONAL INC.506 2ND AVE  STE 140098104"
+python src/bings.py "ALPHACIPHER ACQUISITION CORPORATION401 2ND AVE S STE 50098104"
+python src/bings.py "ALSTON, COURTNAGE, MACAULAY & PROCTOR LLP1000 2ND AVE STE 390098104"
+python src/bings.py "ALTA PLANNING + DESIGN615 2ND AVE STE 60098104"
+python src/bings.py "ALTUS ALLIANCE LLC719 2ND AVE STE 140498104"
+python src/bings.py "ALVARADO CONSULTING701 FIFTH AVENUE 42ND FLOOR98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "2030 EIGHTH AVENUE JOINT VENTURE506 2ND AVE STE 102098104"
+python src/bings.py "AMERICAN PROPERTY CONSULTANTS401 2ND AVE S #62098104"
+python src/bings.py "ANDERTON LAW OFFICE, LLC705 2ND AVE STE 100098104"
+python src/bings.py "ANDREW BERGH, INC., P.S.705 2ND AVE STE 120098104"
+python src/bings.py "ANDREW D CLAPP801 2ND AVE STE 130098104"
+python src/bings.py "ANIMAL NEWS NETWORK220 2ND AVE. S.  # 3198104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ANOINTED ENTERPRISES LLC506 2ND AVE  STE 140098104"
+python src/bings.py "APPLIED AMBIGUITIES406 2ND AVE EXT S # 30398104"
+python src/bings.py "2ND AVE DELI & SUNDRIES1000 2ND AVE STE 10498104"
+python src/bings.py "ARIES APP, INC.220 2ND AVE S STE 20798104"
+python src/bings.py "ARNOLD J BARER1000 2ND AVE  STE 156098104"
+python src/bings.py "ARQLITE SPC220 2ND AVE S98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ARUP NORTH AMERICA LIMITED719 2ND AVE STE 40098104"
+python src/bings.py "ASCENT LAW PARTNERS LLP719 2ND AVE STE 115098104"
+python src/bings.py "ASHOKA SEATTLE220 2ND AVE S # 10298104"
+python src/bings.py "ASIA GINGER TERIYAKI209 2ND AVE S98104"
+python src/bings.py "ASR21, INC.1000 2ND AVE STE 370098104"
+python src/bings.py "ATTENDIBLE, INC.821 2ND AVE STE 80098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "AUKEMA & ASSOCIATES, INC.506 2ND AVE STE 60098104"
+python src/bings.py "AVADEN BIOSCIENCES, INC.1000 2ND AVE STE 324098104"
+python src/bings.py "B&F CAPITAL MARKETS, INC.801 2ND AVE STE 80098104"
+python src/bings.py "B. JEFFREY CARL, ATTORNEY AT LAW705 2ND AVE STE 91098104"
+python src/bings.py "B9 ARCHITECTS INC.610 2ND AVE98104"
+python src/bings.py "BACKSTOP STUDIO1000 2ND AVE STE 120098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "BADER MARTIN1000 2ND AVE STE 340098104"
+python src/bings.py "BANYAN LEGAL COUNSEL, LLP506 2ND AVE  STE 140098104"
+python src/bings.py "BARKER MARTIN, P.S.719 2ND AVE # 120098104"
+python src/bings.py "BARNEY'S JEWELRY & LOAN, LLC401 2ND AVE EXTENSION S98104"
+python src/bings.py "BARRETT & GILMAN1000 2ND AVE STE 350098104"
+python src/bings.py "BARRETTO PACIFIC CORPORATION1000 2ND AVE STE 242098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "BAUMAN-BECIA LAW GROUP801 2ND AVE 1500 NORTON BLDG98104"
+python src/bings.py "BCSP EXCHANGE PROPERTY LLC821 2ND AVE STE 10598104"
+pgrep phantomjs | xargs kill
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 8
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 9
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 10
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 11
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 12
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 13
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 14
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 15
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 16
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 17
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 18
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 19
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 20
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 21
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Madison 22
+pgrep phantomjs | xargs kill
+python src/bings.py "206 BURGER COMPANY823 3RD AVE STE 10698104"
+python src/bings.py "2FIBERFRIENDS LLC106 3RD AVE S98104"
+python src/bings.py "1015 THIRD AVENUE PARKING CORPORATION1015 3RD AVE 12TH FLR98104"
+python src/bings.py "14331 PHINNEY AVE N LLC114 ALASKAN WAY S # 30498104"
+python src/bings.py "206 STAGING LLC114 ALASKAN WAY S 40698104"
+python src/bings.py "GERALD WU HAIR DESIGN114 ALASKAN WAY S STE 60498104"
+pgrep phantomjs | xargs kill
+python src/bings.py "GET A SWEATER114 ALASKAN WAY S  APT 70698104"
+python src/bings.py "HANJIN SHIPPING AMERICA, LLC401 ALASKAN WAY S # 4698104"
+python src/bings.py "HANJIN SHIPPING COMPANY, LTD.401 ALASKAN WAY S # T-4698104"
+python src/bings.py "HCH114 ALASKAN WAY S APT 40698104"
+python src/bings.py "INVENT WORKSHOP INC114 ALASKAN WAY S STE 20098104"
+python src/bings.py "IVAR'S ACRES OF CLAMS #0011007 ALASKAN WAY PIER 5498104"
+pgrep phantomjs | xargs kill
+python src/bings.py "JACKSON FISH MARKET620 ALASKAN WAY STE 20198104"
+python src/bings.py "JEFF GOBLE PHOTOGRAPHY114 ALASKAN WY S STE 30498104"
+python src/bings.py "JEFFREY ENGELSTAD PHOTOGRAPHER114 ALASKAN WAY S APT 60198104"
+python src/bings.py "JNG GLOBAL GOODS222 ALASKAN WAY S APT 198104"
+python src/bings.py "KIDD VALLEY HAMBURGERS1001 ALASKAN WAY STE 10498104"
+python src/bings.py "KOREAN AIR LINESTERMINAL 46 401 ALASKAN WAY98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "KV HOLDINGS, INC.1001 ALASKAN WY98104"
+python src/bings.py "LARRY GILPIN PHOTOGRAPHY114 ALASKAN WAY S # 30398104"
+python src/bings.py "LAURA ELLIS ELSBETH114 ALASKAN WY S # 60298104"
+python src/bings.py "LDC EXPORT MERCHANDISING LLC955 ALASKAN WAY PIER 8698104"
+python src/bings.py "LDC WASHINGTON LLC955 ALASKAN WAY PIER 8698104"
+python src/bings.py "LOU CUEVAS PHOTOGRAPHY114 ALASKAN WAY S STE 40198104"
+pgrep phantomjs | xargs kill
+python src/bings.py "LUXCOLLECTIVE114 ALASKAN WAY S  STE 40198104"
+python src/bings.py "M V KITTITASALASKAN WAY PIER 52 COLMAN DOCK98104"
+python src/bings.py "M V SEALTHALASKAN WAY PIER 52 COLMAN DOCK98104"
+python src/bings.py "M V WALLA WALLAALASKAN WAY PIER 52 COLMAN DOCK98104"
+python src/bings.py "BASILISK AND WEASEL212 ALASKAN WAY S APT 20298104"
+python src/bings.py "BIG PEOPLE TOYS110 ALASKAN WAY S98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "BLACK SHEEP RECORDS212 ALASKAN WAY S # 41098104"
+python src/bings.py "BRIANPHOTOH114 ALASKAN WAY S APT 30898104"
+python src/bings.py "BRIDGE CHASSIS SUPPLY, LLC401 ALASKAN WAY S98104"
+python src/bings.py "C & H IMPORTING & EXPORTING CO INC304 ALASKAN WAY S98104"
+python src/bings.py "CAFFE APPASSIONATO - COLMAN DOCK801 ALASKAN WAY - COLEMAN DOCK PIER 5298104"
+python src/bings.py "CAMP RAHH!114 ALASKAN WAY S APT 30898104"
+pgrep phantomjs | xargs kill
+python src/bings.py "CHARLIE SMITH MUSIC212 ALASKAN WAY S # 31098104"
+python src/bings.py "CIRVELL114 ALASKAN WAY S APT 30498104"
+python src/bings.py "CODOR DESIGN, LLC114 ALASKAN WAY S APT 30198104"
+python src/bings.py "COLMAN DOCK NEWS/CANDYLANE801 ALASKAN WAY PIER 5298104"
+python src/bings.py "COMMUTER COMFORTS CAFE' AND WINE BAR801 ALASKAN WAY/COLEMAN DOCK98104"
+python src/bings.py "COMPASS HOUSING ALLIANCE210 ALASKAN WAY S98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "CRAVE PROMOTIONS114 ALASKAN WAY S APT 30698104"
+python src/bings.py "DAPPER, LLC114 ALASKAN WAY S APT 30298104"
+python src/bings.py "DOUGLAS E WILKINSON114 ALASKAN WAY S #60598104"
+python src/bings.py "DRESHA HOME STYLING L.L.C.114 ALASKAN WAY S  APT 40698104"
+python src/bings.py "DUBS, INC.114 ALASKAN WAY S # 10498104"
+python src/bings.py "EMERALD CITY CHARTERS INC.1001 ALASKAN WAY98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "EMERALD CITY GLIDE LLC214 ALASKAN WAY S98104"
+python src/bings.py "EMINATION, INC.114 ALASKAN WAY S # 60398104"
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Washington 8
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Washington 9
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 8
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 9
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 10
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 11
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 12
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 13
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 14
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 15
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 16
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 17
+pgrep phantomjs | xargs kill
+python src/scraping_run.py Western 18
+pgrep phantomjs | xargs kill
+python src/bings.py "FAINE/OLLER PRODUCTIONS, INC.114 ALASKAN WAY S #60198104"
+python src/bings.py "FLOOR 13114 ALASKAN WAY S APT 30198104"
+python src/bings.py "FLUTTER STUDIOS114 ALASKAN WAY S STE 10498104"
+python src/bings.py "G+L STUDIO114 ALASKAN WAY S STE 40398104"
+python src/bings.py "ADULT AND COUPLE SERVICES901 BOREN AVE STE 130098104"
+python src/bings.py "AES PSYCHOLOGICAL SERVICES901 BOREN AVE STE 130098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "ALETHEIA THERAPEUTICS, LLC901 BOREN AVE  STE 70198104"
+python src/bings.py "ALLISON FINE, MSW COUNSELING FOR INNER BALANCE901 BOREN AVE STE 130098104"
+python src/bings.py "AMANDA EVANS, LMHCA901 BOREN AVE STE 130098104"
+python src/bings.py "2001 MANAGEMENT ASSOCIATES INC1012 BOYLSTON AVE98104"
+python src/bings.py "COMSCORE NETWORKS, INC.316 OCCIDENTAL AVE S STE 20098104"
+python src/bings.py "CULLOM GALLERY313 OCCIDENTAL AVE S98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "DAVID NEWSOM PRODUCTION316 OCCIDENTAL AVE S # 40098104"
+python src/bings.py "DAVIDSON GALLERIES313 OCCIDENTAL AVE S98104"
+python src/bings.py "DOME BURGER RESTAURANT111 OCCIDENTAL AVE S98104"
+python src/bings.py "DONALD W LUBY INC PS419 OCCIDENTAL AVE S # 60098104"
+python src/bings.py "EBBETS FIELD FLANNELS, INC.408 OCCIDENTAL AVE S98104"
+python src/bings.py "ECLECTIC PRODIGY520 OCCIDENTAL AVE S  UNIT 30898104"
+pgrep phantomjs | xargs kill
+python src/bings.py "F. X. MCRORY'S STEAK, CHOP & OYSTER HOUSE419 OCCIDENTAL AVE S FIRST FLOOR #60298104"
+python src/bings.py "FAO CORPORATION419 OCCIDENTAL AVE S STE 60998104"
+python src/bings.py "FAVOR AND FLORA520 OCCIDENTAL AVE S UNIT 60998104"
+python src/bings.py "FLAME PRODUCTIONS, INC.419 OCCIDENTAL AVE S STE 40298104"
+python src/bings.py "FOOTWORKS CYCLES, LLC323 OCCIDENTAL AVE S98104"
+python src/bings.py "FOOTWORKS PHYSICAL THERAPY303 OCCIDENTAL AVE S98104"
+pgrep phantomjs | xargs kill
+python src/bings.py "FOUR SEASONS FAMILY ASSETS D, LLC419 OCCIDENTAL AVE S STE 60098104"
+python src/bings.py "FREDERICK HOLMES AND COMPANY309 OCCIDENTAL AVE S98104"
+python src/bings.py "FULL PULL IMPORTS547 OCCIDENTAL AVE S98104"
+python src/bings.py "GH KIM520 OCCIDENTAL AVE S UNIT 40898104"
+python src/bings.py "GOODIES820 OCCIDENTAL98104"
+python src/bings.py "HOUSE OF BLUES CONCERTS INC419 OCCIDENTAL AVE S STE 40298104"
+pgrep phantomjs | xargs kill
+python src/bings.py "HTC AMERICA CONTENT SERVICES, INC.308 OCCIDENTAL AVE S STE 30098104"
+python src/bings.py "HTC AMERICA HOLDING, INC.308 OCCIDENTAL AVE S STE 30098104"
+python src/bings.py "HTC AMERICA INNOVATION, INC.308 OCCIDENTAL AVE S STE 30098104"
+python src/bings.py "HTC AMERICA, INC.308 OCCIDENTAL AVE S STE 30098104"
+python src/bings.py "INTEGRAL GROUP410 OCCIDENTAL AVE S98104"
+python src/bings.py "INTRACORP MANAGEMENT SERVICES LLC419 OCCIDENTAL AVE S STE 30098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "INTRACORP MARKETING AND SALES419 OCCIDENTAL AVE S STE 30098104"
+python src/bings.py "J W T316 OCCIDENTAL AVE98104"
+python src/bings.py "JEFFREY A. MAY, P.C.419 OCCIDENTAL AVE S # 60098104"
+python src/bings.py "JOHN E. YACONETTI ANTIQUES & DECORATION322 OCCIDENTAL AVE S98104"
+python src/bings.py "KOPLIN DEL RIO313 OCCIDENTAL AVE S98104"
+python src/bings.py "LAKE BOREN WEST DEVELOPMENT LLC419 OCCIDENTAL AVE S STE 30098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "MAHARAM FABRIC CORPORATION312 OCCIDENTAL AVE S98104"
+python src/bings.py "MCBRIDE FINANCIAL ADVISORS, LLC419 OCCIDENTAL AVE S # 60098104"
+python src/bings.py "MEDIABRANDS PUBLISHING311 OCCIDENTAL AVE S STE B-20098104"
+python src/bings.py "MMCS LLC419 OCCIDENTAL AVE S STE 30098104"
+python src/bings.py "NIRMAL'S106 OCCIDENTAL AVE S98104"
+python src/bings.py "PACIFIC ADR CONSULTING, LLC316 OCCIDENTAL AVE S STE 50098104"
+pgrep phantomjs | xargs kill
+python src/bings.py "PACIFIC NORTHWEST CAPITAL GROUP, LLC419 OCCIDENTAL AVE S STE 30098104"
+python src/bings.py "PACIFIC RIM ADVERTISING & MARKETING419 OCCIDENTAL AVE S #50998104"
+pgrep phantomjs | xargs kill
