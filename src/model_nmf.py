cf7c0de2dd030ca8b402b6be61e02ae9016d2444
Mike Breen <breen.mr(at)gmail.com>
2017-03-27 12:32:52 -0700

    saves group assignments to csv

--- src/model_nmf.py
+++ src/model_nmf.py
@@ -3,17 +3,15 @@ import cPickle as pickle
 import csv
 import pandas as pd
 import numpy as np
-from numpy.random import rand
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
-from nltk.tokenize.moses import MosesTokenizer
 import time
 
 
 def reconst_mse(target, left, right):
     return (np.array(target - left.dot(right))**2).mean()
 
-def describe_nmf_results(doc_term_mat, W, H, file_pref, n_top_words = 15):
+def describe_nmf_results(feature_words, H, file_pref, top_words = 15):
     print("Reconstruction error: %f") %(reconst_mse(doc_term_mat, W, H))
     c = []
     with open (file_pref + '_' + str(H.shape[0]) + '_describe_results.txt', 'w+') as f:
@@ -21,7 +19,7 @@ def describe_nmf_results(doc_term_mat, W, H, file_pref, n_top_words = 15):
     for topic_num, topic in enumerate(H):
         a = "Topic %d: " % topic_num
         b = (" ".join([feature_words[i] \
-                for i in topic.argsort()[:-n_top_words - 1:-1]]))
+                for i in topic.argsort()[:-top_words - 1:-1]]))
         c = a + b + ' \n'
         print c
         f = open (file_pref + '_' + str(H.shape[0]) + '_describe_results.txt', 'a+')
@@ -29,49 +27,44 @@ def describe_nmf_results(doc_term_mat, W, H, file_pref, n_top_words = 15):
         f.close()
     return
 
-def my_nmf(doc_term_mat, n_components, n_iterations=50, eps=1e-6):
-    n_rows, n_cols = doc_term_mat.shape
-    W = rand(n_rows*n_components).reshape([n_rows, n_components])
-    H = rand(n_components*n_cols).reshape([n_components, n_cols])
-    dense_doc_term_mat = doc_term_mat.todense()
-    for i in range(n_iterations):
-        H = np.linalg.lstsq(W, dense_doc_term_mat)[0].clip(eps)
-        W = np.linalg.lstsq(H.T, dense_doc_term_mat.T)[0].clip(eps).T
-    return np.array(W), np.array(H)
 
 if __name__ == "__main__":
-    n_features = 15000
-    start = time.time()
 
-    #file_pref = 'model/sea_5000'
+n_features = 15000
+start = time.time()
+
+#file_pref = 'model/sea_5000'
     file_pref = 'model/mini_100'
-    with open(file_pref + '_vectorizer_links.pkl') as f:
-        vectorizer_pkl = pickle.load(f)
     with open(file_pref + '_links_list.pkl') as f:
         documents = pickle.load(f)
-    with open(file_pref + '_doc_term_mat_links.pkl') as f:
-        doc_term_mat = pickle.load(f)
-    with open(file_pref + '_cos_links.pkl') as f:
-        cos = pickle.load(f)
+    #with open(file_pref + '_vectorizer_links.pkl') as f:
+    #    vectorizer_pkl = pickle.load(f)
+    #with open(file_pref + '_doc_term_mat_links.pkl') as f:
+    #    doc_term_mat = pickle.load(f)
 
-    print 'start up:' , time.time() - start
+    print 'Files loaded in:' , time.time() - start
 
-    for n_topics in range(1,5):
+    vectorizer = TfidfVectorizer(decode_error='ignore', max_features=n_features, stop_words='english')
+    doc_term_mat = vectorizer.fit_transform(documents)
+    print 'Time for Features:' , time.time() - start
 
 
-        feature_words = vectorizer_pkl.get_feature_names()
-        print 'Time for Features:' , time.time() - start
+    for n_topics in range(7,8):
 
         print("\n\n---------\nsklearn decomposition")
-        nmf = NMF(n_components=n_topics, random_state=73,
-          alpha=.1, l1_ratio=.5).fit(doc_term_mat)
-        W_sklearn = nmf.transform(doc_term_mat)
-        with open(file_pref + '_links_nmf_fit_trans_' + str(n_topics) + '.pkl', 'wb') as fp:
-            pickle.dump(W_sklearn, fp)
-        H_sklearn = nmf.components_
-        print 'Time for fit transform:' ,  time.time() - start
+        nmf = NMF(n_components=n_topics, random_state=73, alpha=.1,
+            l1_ratio=.5).fit(doc_term_mat)
 
-        print("\n\n---------\nMy decomposition")
-        W_mine, H_mine = my_nmf(doc_term_mat, n_components=n_topics, n_iterations=50, eps=1e-6)
-        describe_nmf_results(doc_term_mat, W_mine, H_mine, file_pref)
-        print 'Time for fit transform:' , time.time() - start
+        W = nmf.transform(doc_term_mat)
+        with open(file_pref + '_links_nmf_fit_trans_' + str(n_topics) + '.pkl', 'wb') as fp:
+            pickle.dump(W, fp)
+        H = nmf.components_
+        print 'Time for fit transform:' ,  time.time() - start
+        feature_words = vectorizer.get_feature_names()
+        describe_nmf_results(feature_words, H, file_pref, top_words = 15)
+        with open(file_pref + '_links_nmf_fit_trans_' + str(n_topics) + '.pkl', 'wb') as fp:
+            pickle.dump(W, fp)
+        group_nmf = W.argmax(axis=1)
+        df = pd.read_csv(file_pref + '_all_train.csv')
+        df[['Groups']] = pd.DataFrame(data=group_nmf)
+        df.to_csv(file_pref + '_group_assingments')
