6362ed8dad6fe45a255a640238d04fdf95c6f269
Mike Breen <breen.mr(at)gmail.com>
2017-03-26 20:01:03 -0700

    saves results to text file

--- src/model_nmf.py
+++ src/model_nmf.py
@@ -3,62 +3,69 @@ import cPickle as pickle
 import csv
 import pandas as pd
 import numpy as np
+from numpy.random import rand
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF
+from nltk.tokenize.moses import MosesTokenizer
+import time
+
 
 def reconst_mse(target, left, right):
     return (np.array(target - left.dot(right))**2).mean()
 
-def describe_nmf_results(document_term_mat, W, H, n_top_words = 15):
-    print("Reconstruction error: %f") %(reconst_mse(document_term_mat, W, H))
+def describe_nmf_results(doc_term_mat, W, H, n_top_words = 15):
+    print("Reconstruction error: %f") %(reconst_mse(doc_term_mat, W, H))
+    with open (file_pref + '_' + str(H.shape[0]) + '_describe_results.txt', 'w') as f:
+        f.write("")
     for topic_num, topic in enumerate(H):
-        print("Topic %d:" % topic_num)
-        print(" ".join([feature_words[i] \
+        a = "Topic %d:" % topic_num
+        b = (" ".join([feature_words[i] \
                 for i in topic.argsort()[:-n_top_words - 1:-1]]))
+        with open (file_pref + '_' + str(H.shape[0]) + '_describe_results.txt', 'a') as f:
+            f.write(a + b)
+        print a
+        print b
     return
 
-def my_nmf(document_term_mat, n_components=15, n_iterations=50, eps=1e-6):
-    n_rows, n_cols = document_term_mat.shape
+def my_nmf(doc_term_mat, n_components, n_iterations=50, eps=1e-6):
+    n_rows, n_cols = doc_term_mat.shape
     W = rand(n_rows*n_components).reshape([n_rows, n_components])
     H = rand(n_components*n_cols).reshape([n_components, n_cols])
     # linalg.lstsq doesn't work on sparse mats
-    dense_document_term_mat = document_term_mat.todense()
+    dense_doc_term_mat = doc_term_mat.todense()
     for i in range(n_iterations):
-        H = linalg.lstsq(W, dense_document_term_mat)[0].clip(eps)
-        W = linalg.lstsq(H.T, dense_document_term_mat.T)[0].clip(eps).T
-    return array(W), array(H)
+        H = np.linalg.lstsq(W, dense_doc_term_mat)[0].clip(eps)
+        W = np.linalg.lstsq(H.T, dense_doc_term_mat.T)[0].clip(eps).T
+    return np.array(W), np.array(H)
 
 if __name__ == "__main__":
-    #n_features = 4000
-    #for i in range(15):
-    #    n_topics = 5
+    n_features = 15000
+    n_topics = 5
+    start = time.time()
 
-    filename = 'thurs_1940.pkl'
-    with open(filename) as f_un:
-        model_unpickled = pickle.load(f_un)
+    file_pref = 'model/sea_5000'
+    with open(file_pref + '_vectorizer_links.pkl') as f:
+        vectorizer_pkl = pickle.load(f)
+    with open(file_pref + '_links_list.pkl') as f:
+        documents = pickle.load(f)
+    with open(file_pref + '_doc_term_mat_links.pkl') as f:
+        doc_term_mat = pickle.load(f)
+    with open(file_pref + '_cos_links.pkl') as f:
+        cos = pickle.load(f)
 
-    doc_bodies=[]
-    with open(filename[0:-4] + 'text_train.txt', 'rb') as f:
-    #with open (filename[0:-4] + 'text_test.csv', 'w') as f:
-        for i in f:
-            doc_bodies.append(i)
+    print 'start up:' , time.time() - start
 
-    doc_bodies = [i.replace('\n', "") for i in doc_bodies]
+    for n_topics in range(1,4):
+        feature_words = vectorizer_pkl.get_feature_names()
+        print 'Time for Features:' , time.time() - start
 
-    n_features = 7000
-    for i in range(15):
-        n_topics = i  #5
-    #vectorizer = CountVectorizer(max_features=n_features)
-    vectorizer = TfidfVectorizer(max_features=n_features, stop_words='english')
-    document_term_mat = vectorizer.fit_transform(doc_bodies)
-    feature_words = vectorizer.get_feature_names()
+        print("\n\n---------\nsklearn decomposition")
+        nmf = NMF(n_components=n_topics)
+        W_sklearn = nmf.fit_transform(doc_term_mat)
+        H_sklearn = nmf.components_
+        print 'Time for fit transform:' ,  time.time() - start
 
-    print("\n\n---------\nsklearn decomposition")
-    nmf = NMF(n_components=n_topics)
-    W_sklearn = nmf.fit_transform(document_term_mat)
-    H_sklearn = nmf.components_
-    describe_nmf_results(document_term_mat, W_sklearn, H_sklearn)
-
-    print("\n\n---------\nMy decomposition")
-    W_mine, H_mine = my_nmf(document_term_mat, n_components=n_topics, n_iterations=50, eps=1e-6)
-    describe_nmf_results(document_term_mat, W_mine, H_mine)
+        print("\n\n---------\nMy decomposition")
+        W_mine, H_mine = my_nmf(doc_term_mat, n_components=n_topics, n_iterations=50, eps=1e-6)
+        describe_nmf_results(doc_term_mat, W_mine, H_mine)
+        print 'Time for fit transform:' , time.time() - start
