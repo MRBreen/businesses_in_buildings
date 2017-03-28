ab77b321ef24839c3679e2a6572f416b7278fd9e
Mike Breen <breen.mr(at)gmail.com>
2017-03-28 15:42:45 -0700

    created doc strings and removed commented out code and spaces

--- src/data_prep.py
+++ src/data_prep.py
@@ -4,12 +4,8 @@ import numpy as np
 import pandas as pd
 import unicodedata
 import string
-
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
-
-from gensim import corpora, models, similarities
-
 from string import punctuation
 
 def read_mongo(db, collection, max=0, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
@@ -36,15 +32,21 @@ def clean_df(df):
               'ALGOSNAP INC.111 S JACKSON ST98104']
     for d in delete_close_duplicates:
         df = df[df['Bus Search'].str.contains(d) != True]
-
     return df
 
-def remove_non_ascii(text):
-    """
-    """
-    CHARSET = set(string.lowercase + string.uppercase + string.whitespace + '.')
-    #return ''.join(c for c in text if c in CHARSET)
+def get_y_labels(df):
+    """Creates identification labels, pseudo-y, to track preselected Companies"""
+    interest_group = pd.read_csv('data/interest_group.csv', header=0)
+    df_i = interest_group[['Bus Search']]
+    df_i.drop(['Bus Search'] == 'BRICKMAN SOUTH JACKSON LLC 111 S JACKSON Seattle')
+    df_i['Tech Company']  = 1
+    df_merged = pd.merge(df, df_i, how='left')
+    df_merged = df_merged.fillna(0)
+    return(df_merged)
 
+def remove_non_ascii(text):
+    """Steps through every character and replaces non_ascii with white space"""
+    CHARSET = set(string.lowercase + string.uppercase + string.whitespace + '.')
     out_row = []
     for sublist in text:
         a = ''
@@ -57,9 +59,7 @@ def remove_non_ascii(text):
     return(out_row)
 
 def clean_links(links):
-    """
-    """
-
+    """Parces a long link down to the first non-www part of the address"""
     clean = []
     for sublist in links:
         a = ''
@@ -78,18 +78,14 @@ def clean_links(links):
     return(clean)
 
 def tokenize_and_normalize(chunks):
-    """Returns stripped down words
-    """
+    """Returns stripped down words"""
     words = [ word_tokenize(sent) for sent in sent_tokenize(chunks) ]
     flatten = [ inner for sublist in words for inner in sublist ]
     stripped = []
-
     for word in flatten:
         if word not in stopwords.words('english'):
             try:
                 stripped.append(word.encode('latin-1').decode('utf8').lower())
             except:
-                #print "Cannot encode: " + word
                 pass
-
     return [ word for word in stripped if len(word) > 1 ]
