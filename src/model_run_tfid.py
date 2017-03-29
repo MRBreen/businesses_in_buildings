8ef50922cf0899a88e68c82ec5278db4bfdc7761
Mike Breen <breen.mr(at)gmail.com>
2017-03-28 21:45:20 -0700

    updated fuction call to tracking_labels to match update on data_prep

--- src/model_run_tfid.py
+++ src/model_run_tfid.py
@@ -2,9 +2,10 @@
 import pandas as pd
 from src.data_prep import read_mongo
 from src.data_prep import clean_df
-from src.data_prep import get_y_labels
+from src.data_prep import tracking_labels
 from src.data_prep import remove_non_ascii
 from src.data_prep import clean_links
+from src.data_prep import flatten
 from src.model_nmf import describe_nmf_results
 import string
 from sklearn.model_selection import train_test_split
@@ -17,24 +18,32 @@ import time
 
 
 if __name__=='__main__':
-
-
+    """Creates excel document of cleaned data and pickle files
+    """
     source = 'mongo'
-    doc_source = 'links'  # 'links' else defaults to text
+    doc_source = 'text'  # 'links' else defaults to text
     file_pref = 'model/mini_100.pkl' #
 
     if source =='mongo':
-        df = read_mongo('wa', 'bing', max=0)
+        df = read_mongo('wa', 'bing', max=50)
         df = clean_df(df)
-        df = get_y_labels(df)
+        df = tracking_labels(df)
         df_train, df_test = train_test_split(df, test_size=0)
 
-        links = [x for x in df_train['Links'].values]
-        links = [remove_non_ascii(link) for link in links]
-        links = clean_links(links)
 
-        text = [x for x in df_train['Text'].values[0]]
-        text = remove_non_ascii(text)
+        links_raw = [x for x in df_train['Links'].values]
+        print 'links in lambda' ,links_raw
+        #links = [remove_non_ascii(link) for link in links]
+        links = map(clean_links, links_raw)
+        print '\nclean \n' ,links
+        print "type(links) " ,type(links)
+        print "type(links[4]) ", type(links[4])
+
+        text = df_train['Text']
+        text = map(flatten, text)
+        print 'type(text)' , type(text)
+        #text = remove_non_ascii(text)
+        print 'type(text[4])' ,type(text[4])
 
         df_train.to_csv(file_pref + '_all_train.csv')
         df_test.to_csv(file_pref + '_all_test.csv')
@@ -42,33 +51,26 @@ if __name__=='__main__':
             pickle.dump(text, fp)
         with open(file_pref + '_links_list.pkl', 'wb') as fp:
             pickle.dump(links, fp)
+        print "Number of docs: " , len(links)
+        print "Date is cleaned, and 4 files written...starting model"
 
     if source != 'mongo':
         filename = sys.argv(1)
         df_train = pd.read_csv(file_pref + '_train.csv')
         df_test = pd.read_csv(file_pref + '_test.csv')
 
-    if doc_source == 'links':
-        documents = links
-    else:
-        documents = text
-
-    print "Number of docs: " , len(links)
-
-    print "Date is cleaned, and 5 files written...starting model"
-
-    if source != 'mongo':
-        df_train.to_csv(file_pref + '_all_train.csv')
-        df_test.to_csv(file_pref + '_all_test.csv')
-
     #if text, the tokenizer has minimum of 4 letters, else default of 2 letters.
     start = time.time()
-    if documents == text:
-        vectorizer = TfidfVectorizer(decode_error='ignore', token_pattern=r'\b\w[a-zA-Z]{2,}\w+\b', stop_words='english')
-    else:
+    if doc_source == 'links':
+        documents = links
         vectorizer = TfidfVectorizer(decode_error='ignore', stop_words='english')
-    print 'vect finished: ' , time.time() - start
+        print vectorizer.idf_
+    else:
+        documents = text
+        vectorizer = TfidfVectorizer(decode_error='ignore', token_pattern=r'\b\w[a-zA-Z]{2,}\w+\b', stop_words='english')
 
+    print 'Vectorizer model instance ready: ' , time.time() - start
+    #print documents
     doc_term_mat = vectorizer.fit_transform(documents)
     with open(file_pref + '_doc_term_mat_links.pkl', 'w') as f:
         pickle.dump(doc_term_mat, f)
@@ -81,4 +83,4 @@ if __name__=='__main__':
     print 'cos similarity finished...dumping to pkl: ' , time.time() - start
     with open(file_pref + '_cos_links.pkl', 'w') as f:
         pickle.dump(cos_sims, f)
-    print 'done. daved to pkl: ' , time.time() - start
+    print 'done. saved to pkl: ' , time.time() - start
