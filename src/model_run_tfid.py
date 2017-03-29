163c7694611da3ce3a5c59dcd25a8bcfeb88f49e
Mike Breen <breen.mr(at)gmail.com>
2017-03-28 21:53:05 -0700

    removed use of df_test / df_train

--- src/model_run_tfid.py
+++ src/model_run_tfid.py
@@ -3,15 +3,12 @@ import pandas as pd
 from src.data_prep import read_mongo
 from src.data_prep import clean_df
 from src.data_prep import tracking_labels
-from src.data_prep import remove_non_ascii
 from src.data_prep import clean_links
 from src.data_prep import flatten
 from src.model_nmf import describe_nmf_results
 import string
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-from nltk.tokenize.moses import MosesTokenizer
 import cPickle as pickle
 import csv
 import time
@@ -28,25 +25,20 @@ if __name__=='__main__':
         df = read_mongo('wa', 'bing', max=50)
         df = clean_df(df)
         df = tracking_labels(df)
-        df_train, df_test = train_test_split(df, test_size=0)
 
-
-        links_raw = [x for x in df_train['Links'].values]
+        links_raw = [x for x in df['Links'].values]
         print 'links in lambda' ,links_raw
-        #links = [remove_non_ascii(link) for link in links]
         links = map(clean_links, links_raw)
         print '\nclean \n' ,links
         print "type(links) " ,type(links)
         print "type(links[4]) ", type(links[4])
 
-        text = df_train['Text']
+        text = df['Text']
         text = map(flatten, text)
         print 'type(text)' , type(text)
-        #text = remove_non_ascii(text)
         print 'type(text[4])' ,type(text[4])
 
-        df_train.to_csv(file_pref + '_all_train.csv')
-        df_test.to_csv(file_pref + '_all_test.csv')
+        df.to_csv(file_pref + '_all.csv')
         with open(file_pref + '_text_list.pkl', 'wb') as fp:
             pickle.dump(text, fp)
         with open(file_pref + '_links_list.pkl', 'wb') as fp:
@@ -56,8 +48,7 @@ if __name__=='__main__':
 
     if source != 'mongo':
         filename = sys.argv(1)
-        df_train = pd.read_csv(file_pref + '_train.csv')
-        df_test = pd.read_csv(file_pref + '_test.csv')
+        df = pd.read_csv(file_pref + '_train.csv')
 
     #if text, the tokenizer has minimum of 4 letters, else default of 2 letters.
     start = time.time()
