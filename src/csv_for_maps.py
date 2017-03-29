844e7c13a7ae3c6312d92009ee95829ade510398
Mike Breen <breen.mr(at)gmail.com>
2017-03-29 00:03:06 -0700

    updated code to be more pythonic...still didn't get rid of unwanted quotes in the csv file

--- src/csv_for_maps.py
+++ src/csv_for_maps.py
@@ -8,6 +8,8 @@ import time
 
 if __name__=='__main__':
     """Creates a cvs file for use with google maps.
+    Data from the business records are querried and merged
+    with output of a model
     """
 
     start_file = 'model/alpha_5500.pkl' ##file to be read: _group_assignments.csv')
@@ -33,11 +35,11 @@ if __name__=='__main__':
     zip_keep = ['98101', '98102', '98102', '98134', '98121']
     df = df[df['Zip'].isin(zip_keep)]
     df["Bus Name"] = df["Bus Name"].astype(str)
-    df["Bus Name"] = df["Bus Name"].map(lambda x: x.replace('"',''))
-    df["Bus Name"] = df["Bus Name"].map(str.strip)
+    df["Bus Name"].str.replace('"','')
+    df["Bus Name"].str.strip()
     df["Address"] = df["Address"].astype(str)
-    df["Address"] = df["Address"].map(lambda x: x.replace('"',''))
-    df["Address"] = df["Address"].map(str.strip)
+    df["Address"].str.replace('"','')
+    df["Address"].str.strip()
     df = df.drop_duplicates(subset='Bus Name')
     for i in range(df.Groups.max() + 1):
         df[df['Groups'] == i].to_csv(file_pref + '_maps_' + str(i) + '.csv')
