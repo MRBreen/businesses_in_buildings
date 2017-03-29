ebf5bf12c47f93c212c4c842f4f94338fdf51449
Mike Breen <breen.mr(at)gmail.com>
2017-03-28 21:27:50 -0700

    created flatten function to flatten the lists inside the text list

--- src/data_prep.py
+++ src/data_prep.py
@@ -9,8 +9,7 @@ from nltk.corpus import stopwords
 from string import punctuation
 
 def read_mongo(db, collection, max=0, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
-    """ read from Mongo and Store into DataFrame
-    """
+    """Returns a DataFrame"""
     client = MongoClient()
     client = MongoClient('mongodb://localhost:27017/')
     db = client[db]
@@ -23,8 +22,7 @@ def read_mongo(db, collection, max=0, query={}, host='localhost', port=27017, us
     return df
 
 def clean_df(df):
-    """ cleans up df for all models
-    """
+    """Returns a dataframe without n/a and duplicates."""
     df = df.dropna()
     df = df.drop_duplicates(subset='Bus Search')
     delete_close_duplicates = ['UPSIDE COMMERCE, INC. 111 S JACKSON ST #451', 'UPSIDE 111 s Jackson',
@@ -35,7 +33,7 @@ def clean_df(df):
     return df
 
 def get_y_labels(df):
-    """Creates identification labels, pseudo-y, to track preselected Companies"""
+    """Creates identification labels, pseudo-y, to track preselected Companies."""
     interest_group = pd.read_csv('data/interest_group.csv', header=0)
     df_i = interest_group[['Bus Search']]
     df_i.drop(['Bus Search'] == 'BRICKMAN SOUTH JACKSON LLC 111 S JACKSON Seattle')
@@ -45,7 +43,7 @@ def get_y_labels(df):
     return(df_merged)
 
 def remove_non_ascii(text):
-    """Steps through every character and replaces non_ascii with white space"""
+    """Steps through every character and replaces non_ascii with white space."""
     CHARSET = set(string.lowercase + string.uppercase + string.whitespace + '.')
     out_row = []
     for sublist in text:
@@ -59,7 +57,7 @@ def remove_non_ascii(text):
     return(out_row)
 
 def clean_links(links):
-    """Parces a long link down to the first non-www part of the address"""
+    """Parces a long link down to the first non-www part of the address."""
     flatten = [item for sublist in links for item in sublist]
     flatten = str(flatten)
     text = string.replace(flatten, '\\' ,  "/" )
@@ -80,8 +78,18 @@ def clean_links(links):
         text = text[text.find(", u'")+4:]
     return(" ".join(clean))
 
+def flatted(text):
+    """Removes lists inside the data for other functions to use. """
+    flat = []
+    for page in text:
+        a = []
+        for row in page:
+            a.append(row[1])
+        flat.append(" ".join(a))
+    return(flat)
+
 def tokenize_and_normalize(chunks):
-    """Returns stripped down words"""
+    """Returns stripped down words."""
     words = [ word_tokenize(sent) for sent in sent_tokenize(chunks) ]
     flatten = [ inner for sublist in words for inner in sublist ]
     stripped = []
